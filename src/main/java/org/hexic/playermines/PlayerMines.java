02a7a776768d34f06f838f4cc6a066d0d01f2383
Siiant <smsklotz@gmail.com>
2021-09-05 22:35:04 -0400

    Initial commit. Basic project structure.

---
+++ src/main/java/org/hexic/playermines/PlayerMines.java
@@ -0,0 +1,17 @@
+package org.hexic.playermines;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerMines extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
