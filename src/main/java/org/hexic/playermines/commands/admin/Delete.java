fca3c658a6dd8a3b07815e2b242032e8599a4123
Siiant <siiantdev@gmail.com>
2022-05-24 15:04:30 -0400

    Refactoring. Changed how schematics are loaded. Updated README.md.

---
+++ src/main/java/org/hexic/playermines/commands/admin/Delete.java
@@ -0,0 +1,39 @@
+package org.hexic.playermines.commands.admin;
+
+import org.bukkit.entity.Player;
+import org.hexic.playermines.PlayerMine.PlayerMine;
+import org.hexic.playermines.commands.manager.SubCommand;
+
+public class Delete extends SubCommand {
+    @Override
+    public String getName() {
+        return "Delete";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Delete the selected Players Mine and associated data.";
+    }
+
+    @Override
+    public void perform(Player player, String[] args) {
+        if(args.length < 3){
+            //Not enough args
+            player.sendMessage("Not enough args.");
+        } else {
+            if(new PlayerMine(args[2]).hasMine()){
+                new PlayerMine(args[2]).delete();
+                player.sendMessage("Deleted.");
+            } else {
+                //No mine
+                player.sendMessage("No Mine");
+            }
+        }
+
+    }
+
+    @Override
+    public String getPermission() {
+        return "PlayerMines.Admin.Delete";
+    }
+}
